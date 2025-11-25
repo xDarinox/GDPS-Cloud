104625775a4b23ec8cea640943ebd9193a55e779
xDarinox <kirillmitrofanov2006@mail.ru>
2025-11-25 04:08:22 +0300

    Add files via upload

--- core/src/main/errorManager.php
+++ core/src/main/errorManager.php
@@ -3,7 +3,7 @@
     public static function accessDenied()
     {
         $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-        $currentURL = ($_SERVER[REQUEST_URI] == '/') ? "<cl>https://$_SERVER[HTTP_HOST]</cl>" : "<cg>https://$_SERVER[HTTP_HOST]</cg><cy>$_SERVER[REQUEST_URI]</cy>";
+        $currentURL = ($_SERVER[REQUEST_URI] == '/') ? "<cg>https://$_SERVER[HTTP_HOST]</cg>" : "<cg>https://$_SERVER[HTTP_HOST]</cg><cy>$_SERVER[REQUEST_URI]</cy>";
         $text = ($lang == 'ru') ? 'Доступ к ресурсу ограничен!' : 'Access to the resource is denied!';
             
         return "<head>
@@ -18,48 +18,10 @@
                     </div>
                 </body>
                 <style>
-                    @media screen and (orientation: portrait)
-                    {
-                        #image
-                        {
-                            width: 18%;
-                        }
-        
-                        #text
-                        {
-                            font-size: 2.1vh;
-                        }
-        
-                        code
-                        {
-                            font-size: 1.5vh;
-                        }
-                    }
-    
-                    h2
-                    {
-                        font-family: 'Unbounded', sans-serif;
-                        color: #fff;
-                    }
-    
-                    code
-                    {
-                        color: #fff;
-                        padding: 1vh 1.35vh;
-                        background: #0c0c0c;
-                        border-radius: 1vh;
-                    }
-    
-                    #imageText
-                    {
-                        display: flex;
-                        justify-content: center;
-                        flex-direction: column;
-                        height: 100%;
-                        width: 100%;
-                        align-items: center;
-                    }
-    
+                    @media screen and (orientation:portrait) { #image { width:18%; } #text { font-size:2.1vh; } code { font-size:1.5vh; } }
+                    h2 { font-family:'Unbounded',sans-serif;color:#fff; }
+                    code { color:#fff;padding:1vh 1.35vh;background:#0c0c0c;border-radius: 1vh; }
+                    #imageText { display:flex;justify-content:center;flex-direction:column;height:100%;width:100%;align-items:center; }
                     cy { color: #ffb6b6 }
                     cg { color: #ffebb1 }
                 </style>";
